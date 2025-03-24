9f901c3cf70db3e7eebc29572aaf9fd863960c12
zcross13 <zalicross24@gmail.com>
2025-03-23 20:21:43 -0500

    fix local host

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,23 @@
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/rock_with_me/", // Adjust the repository name
+  plugins: [react()],
+  resolve: {
+    alias: {
+      // Create aliases to simplify imports in your code
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    // Optionally configure server options here
+    port: 3000, // You can change the port if needed
+    open: true, // Automatically open the browser when the server starts
+  },
+  build: {
+    // Optionally configure build options here
+    outDir: "dist", // Define the output directory
+  },
 });
