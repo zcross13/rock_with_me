a9493995670d964a80fb80e35a69975d94bc78bb
zcross13 <zalicross24@gmail.com>
2025-03-24 08:13:54 -0500

    fix spacing on mobile header

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
-import './styles.css';
 import { BrowserRouter } from 'react-router-dom';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
