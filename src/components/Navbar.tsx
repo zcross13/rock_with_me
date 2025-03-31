8e61f23111b0629012000bbf3ef80760755a4b28
zcross13 <zalicross24@gmail.com>
2025-03-31 00:38:32 -0500

    hamburger menu styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,17 @@
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 import '../styles/navbar.css';
 
 const Navbar = () => {
+
+  const [isOpen, setIsOpen] = useState(false); // State to control open/close of menu
+
+  // Toggle function for hamburger menu
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+
   return (
     <nav className="navbar">
       {/* Container for both logos */}
@@ -17,22 +27,30 @@ const Navbar = () => {
         </div>
       </div>
 
-      {/* Navigation Links */}
-      <ul className="nav-links">
-        <li><Link to="/about-us" aria-label="About me Page">About</Link></li>
-        <li><Link to="/services" aria-label="Services Page">Services</Link></li>
-        <li><Link to="/gallery" aria-label="Gallery Page">Gallery</Link></li>
-        <li><Link to="/contact" aria-label="Contact Page">Contact</Link></li>
-        <li><Link to="/faq" aria-label="FAQ Page">FAQ</Link></li>
-      </ul>
+      {/* Hamburger Menu */}
+      <div className={`hamburger ${isOpen ? 'active' : ''}`} onClick={toggleMenu}>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+      </div>
 
-      <button
+      {/* Navigation Links */}
+      <ul className={`nav-links ${isOpen ? 'open' : ''}`}>
+        <li><Link to="/about-us" aria-label="About me Page" onClick={() => setIsOpen(false)}>About</Link></li>
+        <li><Link to="/services" aria-label="Services Page" onClick={() => setIsOpen(false)}>Services</Link></li>
+        <li><Link to="/gallery" aria-label="Gallery Page" onClick={() => setIsOpen(false)}>Gallery</Link></li>
+        <li><Link to="/contact" aria-label="Contact Page"  onClick={() => setIsOpen(false)}>Contact</Link></li>
+        <li><Link to="/faq" aria-label="FAQ Page" onClick={() => setIsOpen(false)}>FAQ</Link></li>
+        <li>
+        <button
           className="cta-button"
           onClick={() =>
             (window.location.href =
               'https://app.acuityscheduling.com/schedule/057387c0')
           }
         > Book Now </button>
+        </li>
+      </ul>
     </nav>
   );
 };
