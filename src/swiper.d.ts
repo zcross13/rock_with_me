b88139000c3e5bec5e090509fcc73a1bf0d843f1
zcross13 <zalicross24@gmail.com>
2025-04-26 12:17:15 -0500

    Finished service carousel

---
+++ src/swiper.d.ts
@@ -0,0 +1,8 @@
+// swiper.d.ts
+
+declare module "swiper/css";
+declare module "swiper/css/pagination";
+declare module "swiper/css/autoplay";
+declare module "swiper/css/effect-fade";
+declare module "swiper/css/navigation";
+// Add more modules here as needed
